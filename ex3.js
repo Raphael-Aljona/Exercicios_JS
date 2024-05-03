e0dc542e0ee36de8a120b8a708c53308022e9307
RaphaelAljona <53789144851@info.local>
2024-05-03 15:34:43 -0300

    feat: Exercicio 3

---
+++ ex3.js
@@ -0,0 +1,15 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let n = prompt("Digite um numero");
+let total=n/2;
+
+if(n%2 == 0){
+	console.log("Par");
+}else{
+	console.log("Impar");
+}
+
+
+
