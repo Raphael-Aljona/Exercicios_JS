8406fdd7f0954e47e47dc9859d4b0cd9c11872c0
RaphaelAljona <53789144851@info.local>
2024-05-07 17:06:29 -0300

    feat: Exercicio 4

---
+++ Loop/ex4.js
@@ -0,0 +1,8 @@
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+let n = parseInt (prompt ("Digite um número de 1 a 10"));
+
+for (let tabuada=1; tabuada<=10; tabuada++){
+    console.log (tabuada * n)
+}
