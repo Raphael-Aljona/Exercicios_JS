eef84c48350e39543286bf4a05acae7368fe60ec
RaphaelAljona <53789144851@info.local>
2024-05-08 15:03:14 -0300

    feat: Exercicio 9

---
+++ Loop/ex9.js
@@ -0,0 +1,9 @@
+// 9. Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+let usuario = parseInt (prompt ("Digite um número inteiro: "))
+
+while (usuario >= 1) {
+    usuario --;
+    console.log (usuario)
+}
