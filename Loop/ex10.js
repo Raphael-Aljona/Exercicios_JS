08efb61de5b6e758cec5f5d45353a5e1c8ab4f4f
RaphaelAljona <53789144851@info.local>
2024-05-08 15:09:55 -0300

    feat: Exercicio 10

---
+++ Loop/ex10.js
@@ -0,0 +1,10 @@
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+let n = 0
+
+do {
+    n = parseInt (prompt ("Digite um número inteiro: "))
+}while (n != 0)
+
+// console.log (n)
